c594b244aa865d4d3163704eb8567e80f38acfdc
AO-DEV <ao-dev@Anjis-Mac.local>
2020-04-11 11:07:39 +0530

    HeatMap integration

--- go-corona-app/screens/HomeScreen.js
+++ go-corona-app/screens/HomeScreen.js
@@ -1,138 +1,121 @@
 import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { Component } from 'react';
+import { Image, Platform, Dimensions, StyleSheet, Text, TouchableOpacity, View, ActivityIndicator } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import MapView, { PROVIDER_GOOGLE, Heatmap } from 'react-native-maps';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+
+import data from '../test_data/location_history.js'
+
 
 import { MonoText } from '../components/StyledText';
+import { renderer } from 'react-test-renderer';
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-      </ScrollView>
-    </View>
-  );
+// TODO: move to constants
+const latitudeDelta = 0.09;
+const longitudeDelta = 0.0121;
+
+export default class HomeScreen extends Component {
+  state = {
+    loading: true,
+    region: null,
+    points: [],
+    error: null,
+    mapWidth: '99%'
+  }
+  map = null;
+  async componentDidMount() {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== 'granted') {
+      // TODO: move to string constants
+      this.setState({ error: 'Location permission is needed' })
+    }
+    const location = await Location.getCurrentPositionAsync({});
+    const { latitude, longitude } = location.coords;
+    // TODO: call rest api to get nearby hotspots providing current location details
+    var points = this.getHeatMapPoints(data)
+    this.setHeatMapPoints(latitude, longitude, points);
+  }
+
+  onMapReady = () => {
+    this.map.animateToRegion(this.state.region);
+    setTimeout(() => {
+      this.setState({
+        mapWidth: '100%'
+      })
+    }, 100);
+  }
+
+  setHeatMapPoints(latitude, longitude, points) {
+    this.setState({
+      region: {
+        latitude,
+        longitude,
+        latitudeDelta,
+        longitudeDelta
+      },
+      loading: false,
+      points
+    });
+  }
+
+  getHeatMapPoints(data) {
+    return data.locations.map((location) => {
+      let { latitudeE7, longitudeE7 } = location;
+      if (latitudeE7 > 900000000)
+        latitudeE7 = latitudeE7 - 4294967296;
+      if (longitudeE7 > 1800000000)
+        longitudeE7 = longitudeE7 - 4294967296;
+      let latitude = latitudeE7 / 10000000;
+      let longitude = longitudeE7 / 10000000;
+      return {
+        latitude,
+        longitude
+      };
+    });
+  }
+
+  render() {
+    const { loading, points, mapWidth } = this.state;
+    return (
+      <View style={styles.container}>
+        {this.state.loading ?
+          <ActivityIndicator></ActivityIndicator> :
+          <MapView
+            ref={(ref) => { this.map = ref }}
+            provider={PROVIDER_GOOGLE}
+            onMapReady={this.onMapReady}
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+            style={[styles.map, { width: mapWidth }]}
+          >
+            <Heatmap
+              style={styles.mapStyle}
+              points={points}
+            />
+          </MapView>
+        }
+      </View>
+    );
+  }
+
 }
 
 HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    justifyContent: 'center',
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
+
