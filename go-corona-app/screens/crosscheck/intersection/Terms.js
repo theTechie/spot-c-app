0e9e0387b83d4d1c8ce2d012879dc9f2ed9e69f8
theTechie <gagan.blr@gmail.com>
2020-04-13 21:05:08 +0530

    add more icon svgs + add checkbox for intersection terms

--- go-corona-app/screens/crosscheck/intersection/Terms.js
+++ go-corona-app/screens/crosscheck/intersection/Terms.js
@@ -1,12 +1,15 @@
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { Checkbox } from 'react-native-material-ui'
+
 import TermsImage from '../../../assets/images/IntersectionTerms.svg'
 
+export default function CrosscheckTerms({ onAgree }) {
+  const [agree, setAgree] = useState(false)
 
-export default function CrosscheckTerms() {
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -20,25 +23,10 @@ export default function CrosscheckTerms() {
           - <Text style={styles.boldText}>The results are probabilistic.</Text> Even if you have come in contact with a COVID +ve person, this is only a probabilistic model to know if you have been infected. {'\n'}
           - <Text style={styles.boldText}>Your data is safe.</Text> Information that you provide is anonymous and not shared with anyone.{'\n'}
           </Text>
-          <Text> I read and accept Terms of Service and Privacy Policy</Text>
+          <Checkbox label="I read and accept Terms of Service and Privacy Policy" value="agree" checked={agree} onCheck={val => setAgree(val)} />
         </View>
       </View>
     </ScrollView>
-  );
-}
-
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
   );
 }
 
