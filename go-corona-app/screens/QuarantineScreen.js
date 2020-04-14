61fe3baffd9c962d6932599660621e77fe8de7f3
Harshit Kanodia <harshit.kanodia@checkapp.in>
2020-04-15 00:24:57 +0530

    Adding, Deleting and viewing quarantine days is working fine now

--- go-corona-app/screens/QuarantineScreen.js
+++ go-corona-app/screens/QuarantineScreen.js
@@ -1,34 +1,70 @@
 import { Ionicons } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Modal } from "react-native";
+import DateRangePicker from "../components/DateRangePicker";
 import {
   RectButton,
   ScrollView,
   TouchableOpacity,
+  FlatList,
 } from "react-native-gesture-handler";
 import { CalendarList } from "react-native-calendars";
+import { makeid } from "../utils/helpers";
 
 export default function QuarantineScreen() {
-  const [quarantinedDays, setQuarantinedDays] = React.useState([
-    "2020-04-13",
-    "2020-04-14",
-    "2020-04-15",
-    "2020-04-16",
-    "2020-04-17",
-  ]);
   const [selectedDays, setSelectedDays] = React.useState([
-    { start: "2020-04-12", end: "2020-04-19" },
-    { start: "2020-04-12", end: "2020-04-19" },
+    { start: "2020-04-12", end: "2020-04-19" , key:"abcd1234"},
   ]);
   const CALENDAR = "CALENDAR";
   const DATE_SELECTOR = "DATE_SELECTOR";
   const [currentScreen, setCurrentScreen] = React.useState(CALENDAR);
-  const getQuarantineDays = (quarantinedDays) => {
-    let days = {};
-    quarantinedDays.forEach((day) => (days[day] = { marked: true }));
-    return days;
+
+
+  const getQuarantineMarkedDays = (startDate,endDate,markedDates) => {
+    let deltaDays =
+      (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
+    let start = new Date(startDate);
+    for (var i = 0; i <= deltaDays; i++) {
+      start.setDate(startDate.getDate() + i);
+      console.log(start.getDate());
+      markedDates[formatDate(start)] = {
+        marked: true
+      };
+    }
+    return markedDates;
   };
+
+
+  const formatDate = (date, type) => {
+    let dt = zeroPadNumber(date.getDate())
+    let month = zeroPadNumber(date.getMonth()+1)
+
+    if (type == "IN")
+      return (
+        "" + dt + "-" + month + "-" + date.getFullYear()
+      );
+    else
+      return date.getFullYear() + "-" + month + "-" + dt;
+  };
+
+  const zeroPadNumber = (number)=>{
+    return number < 10 ? "0"+number : ""+number;
+  }
+
+  const getMarkedDaysObject=(selectedDays)=>{
+    let markedDates = {};
+    selectedDays.forEach(({start,end})=>{
+      let startDate = new Date(Date.parse(start));
+      let endDate = new Date(Date.parse(end));
+      console.log('startDate ',startDate);
+      console.log('endDate ',endDate);
+      markedDates = getQuarantineMarkedDays(startDate, endDate,markedDates);
+    });
+    return markedDates;
+  }
+
+  let markedDays = getMarkedDaysObject(selectedDays);
   return (
     <View
       style={styles.container}
@@ -72,7 +108,7 @@ export default function QuarantineScreen() {
                     borderColor: "#818181",
                     backgroundColor:
                       marking instanceof Array ? "transparent" : "#FFCEDF",
-                    opacity: state === "disabled" ? "0.4" : "1",
+                    opacity: state === "disabled" ? 0.4 : 1,
                     width: 42,
                     height: 42,
                     justifyContent: "center",
@@ -90,28 +126,51 @@ export default function QuarantineScreen() {
                 </View>
               );
             }}
-            markedDates={getQuarantineDays(quarantinedDays)}
+            markedDates={markedDays}
           />
         ) : (
-          <AddQuarantineDates selected={selectedDays} />
+          <AddQuarantineDates
+            selected={selectedDays}
+            onRemoveClicked={(index) => {
+              console.log("Remove selectedDays at index ",index);
+              selectedDays.splice(index, 1);
+              let newSelection = [];
+              selectedDays.forEach((s)=>{newSelection.push(s)})
+              setSelectedDays(newSelection);
+            }}
+            onDateRangeSelected={(sd, ed) => {
+              selectedDays.push({
+                'start' : sd,
+                'end' : ed,
+                'key' : makeid(4)
+              })
+              setSelectedDays(selectedDays);
+            }}
+          />
         )}
       </View>
     </View>
   );
 }
 
-function ChooseStartAndEndDate({ type, date, month }) {
+function ChooseStartAndEndDate({ type, date, month, onClick, key }) {
   return (
-    <View style={{ marginTop: 16 }}>
-      <Text style={styles.chooseYourDateTextStyle}>
-        {"Choose your " + type + " date"}
-      </Text>
-      <Divider marginTop={8} />
-      <View marginTop={12} paddingLeft={"5%"} style={{ flexDirection: "row" }}>
-        <DateChip number={date} />
-        <DateChip marginLeft={12} number={month} />
+    <TouchableOpacity onPress={onClick} key={key}>
+      <View style={{ marginTop: 12 }}>
+        <Text style={styles.chooseYourDateTextStyle}>
+          {"Choose your " + type + " date"}
+        </Text>
+        <Divider marginTop={8} />
+        <View
+          marginTop={12}
+          paddingLeft={"5%"}
+          style={{ flexDirection: "row" }}
+        >
+          <DateChip number={date} />
+          <DateChip marginLeft={12} number={month} />
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
@@ -134,48 +193,110 @@ function DateChip({ number, marginLeft }) {
   );
 }
 
-function AddQuarantineDates({ selected }) {
+function AddQuarantineDates({
+  selected,
+  onRemoveClicked,
+  onAddClicked,
+  onDateRangeSelected,
+}) {
+  const [showDatePicker, setShowDatePicker] = React.useState(false);
   return (
     <View style={styles.addQuarantineDatesContainer}>
-      <Text style={{ fontWeight: "bold", fontSize: 16, padding: "5%" }}>
-        When in doubt, Self-Quarantine
+      <Modal visible={showDatePicker} transparent={true}>
+        <View style={styles.modalContainerStyle}>
+          <DateRangePicker
+            onSuccess={(se, ed) => {
+              setShowDatePicker(false);
+              onDateRangeSelected(se, ed);
+            }}
+          />
+        </View>
+      </Modal>
+
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 16,
+          paddingTop: "3%",
+          paddingLeft: "5%",
+          paddingBottom: "3%",
+        }}
+      >
+        {"When in doubt, Self-Quarantine"}
       </Text>
-      <View style={{height:"100%"}}>
-      <ScrollView style={{height:"90%"}}>
-        {selected.map(({ start, end }) => {
-          return (
-            <View>
-              <ChooseStartAndEndDate
-                type="start"
-                month={start.split("-")[1]}
-                date={start.split("-")[2]}
+      <View style={{ height: "100%" }}>
+        <ScrollView style={{ height: "80%" }}>
+          {selected.map(({ start, end }, index) => {
+            return <AddedQuarantineDateRange
+                start={start}
+                end={end}
+                onDeleteClick={() => {
+                  onRemoveClicked(index);
+                }}
               />
-              <ChooseStartAndEndDate
-                type="end"
-                month={end.split("-")[1]}
-                date={end.split("-")[2]}
-              />
-              <DeleteButton item={{ start: start, end: end }} />
-            </View>
-          );
-        })}
-      </ScrollView>
-        <View style={{height:"10%"}}>
-          <Divider/>
-          <View style={{padding:"2%" , justifyContent:"space-between"}}>
-            <Text style={{color:"blue",fontSize:16}}>
-              {"< Back"}
-            </Text>
+          })}
+          <AddMoreButton
+            onClick={() => {
+              setShowDatePicker(true);
+            }}
+          />
+        </ScrollView>
+        <View style={{ height: "10%" }}>
+          <Divider />
+          <View style={{ padding: "2%", justifyContent: "space-between" }}>
+            <Text style={{ color: "blue", fontSize: 16 }}>{"< Back"}</Text>
           </View>
-          <Divider/>
+          <Divider />
         </View>
       </View>
-     
     </View>
   );
 }
 
-function DeleteButton({ item }) {
+function AddMoreButton({ onClick }) {
+  return (
+    <RectButton
+      style={{
+        width: 112,
+        height: 36,
+        backgroundColor: "#A9E7CB",
+        borderColor: "#909090",
+        borderWidth: 1,
+        borderRadius: 8,
+        marginTop: 32,
+        marginLeft: "5%",
+        justifyContent: "center",
+      }}
+      onPress={onClick}
+    >
+      <Text
+        style={{ textAlign: "center", fontWeight: "bold", color: "#989898" }}
+      >
+        ADD
+      </Text>
+    </RectButton>
+  );
+}
+
+function AddedQuarantineDateRange({ start, end, onDeleteClick }) {
+  return (
+    <View>
+      <ChooseStartAndEndDate
+        type="start"
+        month={start.split("-")[1]}
+        date={start.split("-")[2]}
+      />
+      <ChooseStartAndEndDate
+        type="end"
+        month={end.split("-")[1]}
+        date={end.split("-")[2]}
+      />
+      <DeleteButton onClick={onDeleteClick} />
+    </View>
+  );
+}
+
+function DeleteButton({ key, onClick }) {
   return (
     <RectButton
       style={{
@@ -189,6 +310,7 @@ function DeleteButton({ item }) {
         marginLeft: "5%",
         justifyContent: "center",
       }}
+      onPress={onClick}
     >
       <Text
         style={{ textAlign: "center", fontWeight: "bold", color: "#989898" }}
@@ -201,7 +323,7 @@ function DeleteButton({ item }) {
 
 function ScreenHeader({ label, iconName, onClick }) {
   return (
-    <TouchableOpacity onPress={onClick}>
+    <TouchableOpacity onPress={onClick} styles={{ marginTop: 18 }}>
       <View style={styles.headerContainer}>
         <Text style={styles.text}>{label}</Text>
         <Ionicons
@@ -283,5 +405,21 @@ const styles = StyleSheet.create({
   chooseYourDateTextStyle: {
     fontSize: 16,
     paddingLeft: "5%",
+  },
+
+  modalContainerStyle: {
+    margin: "10%",
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
