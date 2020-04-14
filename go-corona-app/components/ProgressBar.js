193262faf10a7d198b3b4d2403d6bb68cd28d8c9
theTechie <gagan.blr@gmail.com>
2020-04-14 18:53:04 +0530

    refactor and add totalPages as prop; without this we were not seeing any progress increase

--- go-corona-app/components/ProgressBar.js
+++ go-corona-app/components/ProgressBar.js
@@ -1,83 +1,72 @@
 import React, { Component } from 'react';
-import { View, Text, Platform, ProgressBarAndroid, ProgressViewIOS, StyleSheet} from 'react-native';
+import { View, Text, Platform, ProgressBarAndroid, ProgressViewIOS, StyleSheet } from 'react-native';
+
 import Separator from "./Separator";
 
-export default class ProgressBarContainer extends Component
-{
-  constructor(props)
-  {
-    super(props);
-    this.state = { progressBarProgress: 0.0, textOnTop: this.props.textOnTop, currPage: this.props.currPage }
-  }
+const ProgressBar = ({ textOnTop, currPage = 1, totalPages = 1 }) => {
+  const progress = currPage / totalPages
 
-  changeProgress = () =>
-  {
-    this.setState({ progressBarProgress: this.state.currPage/11 });
-  }
-
-  componentDidMount()
-  {
-    this.changeProgress();
-  }
-
-  render()
-  {
-    return(
-      <View style = { styles.container }>
+  return (
+    <View style={styles.container}>
       <Separator />
-        <View  style={{flex: 1}}>
-        <Text style = { styles.text }>{this.state.textOnTop}</Text>
-        </View>
-        {
-            ( Platform.OS === 'android' )
-            ?
-              (<View style={styles.progressAndView}><ProgressBarAndroid progress = { this.state.progressBarProgress } styleAttr = "Horizontal" indeterminate = { false } style={styles.progressAnd} /> </View>)
-            :
-              (<View style={styles.progressIosView}><ProgressViewIOS progress = { this.state.progressBarProgress } style ={styles.progressIos} progressTintColor="#49D581" /></View> )
-        }    
+      <View style={{ flex: 1 }}>
+        <Text style={styles.text}>{textOnTop}</Text>
       </View>
-    );
-  }
-}
+      {
+        (Platform.OS === 'android')
+          ?
+          (<View style={styles.progressAndView}>
+            <ProgressBarAndroid progress={progress} styleAttr="Horizontal" indeterminate={false} style={styles.progressAnd} />
+          </View>)
+          :
+          (<View style={styles.progressIosView}>
+            <ProgressViewIOS progress={progress} style={styles.progressIos} progressTintColor="#49D581" />
+          </View>)
+      }
+    </View>
+  );
+};
+
+export default ProgressBar;
 
 const styles = StyleSheet.create(
-{
-  container:
   {
-    flex: 1,
-    flexDirection:'row',
-    justifyContent: 'center',
-    paddingTop: ( Platform.OS === 'ios' ) ? 12 : 0,
-    paddingHorizontal: 25,
-    backgroundColor:'#EEEEEE',
-  },
+    container:
+    {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      paddingTop: (Platform.OS === 'ios') ? 12 : 0,
+      paddingHorizontal: 25,
+      backgroundColor: '#EEEEEE',
+    },
 
-  text:
-  {
-    color: 'black',
-    fontSize: 11,
-    marginBottom: 15,
-    textAlign: 'left'
-  },
-  
-  progressAndView:{
-    flex:1
-  },
-  
-  progressIosView:{
-    flex: 1, 
-    marginTop:6
-  },
+    text:
+    {
+      color: 'black',
+      fontSize: 15,
+      marginBottom: 15,
+      textAlign: 'left'
+    },
 
-  progressAnd:{
-      width:'50%', 
-      color:'#49D581',
-      paddingTop:50
-  },
-  
-  progressIos:{
-    width:'50%', 
-    alignSelf:'flex-end'
-  }
- 
-});
+    progressAndView: {
+      flex: 1
+    },
+
+    progressIosView: {
+      flex: 1,
+      marginTop: 6
+    },
+
+    progressAnd: {
+      width: '50%',
+      color: '#49D581',
+      paddingTop: 50
+    },
+
+    progressIos: {
+      width: '50%',
+      alignSelf: 'flex-end'
+    }
+
+  });
