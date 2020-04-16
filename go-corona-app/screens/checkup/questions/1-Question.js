61f20638a08d5545bba2e60d1daaa6d94ba10f9f
anuroopa18 <tsanuroopa@gmail.com>
2020-04-16 12:29:51 -0500

    Resolving conflicts

--- go-corona-app/screens/checkup/questions/1-Question.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const Question1 = () => (
-  <View><Text>Question 1.</Text></View>
-)
-
-export default Question1
