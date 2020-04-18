7549456c67d1da783d3eb00c981dd1aec4aede69
theTechie <gagan.blr@gmail.com>
2020-04-19 00:47:55 +0530

    change bottomNavIcon color

--- go-corona-app/constants/Colors.js
+++ go-corona-app/constants/Colors.js
@@ -3,7 +3,7 @@ const tintColor = '#2f95dc';
 export default {
   tintColor,
   tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
+  tabIconSelected: '#E03D51',
   tabBar: '#fefefe',
   errorBackground: 'red',
   errorText: '#fff',
