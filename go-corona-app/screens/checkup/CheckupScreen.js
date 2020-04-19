c26a56aaef40825163e901bed235761d4e8fdb1f
AO-DEV <ao-dev@Anjis-Mac.local>
2020-04-19 16:43:40 +0530

    viruses svgs

--- go-corona-app/screens/checkup/CheckupScreen.js
+++ go-corona-app/screens/checkup/CheckupScreen.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { StyleSheet, Alert, ProgressBarAndroid, ProgressViewIOS, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Alert, ProgressBarAndroid, ProgressViewIOS, Text, View, Image, ScrollView, ActivityIndicator } from 'react-native';
 
 import Back from "../../components/stepper/buttons/Back";
 import Next from "../../components/stepper/buttons/Next";
@@ -22,6 +22,8 @@ import SymptomBreathlessNess from './questions/SymptomBreathlessNess.js';
 import SymptomSoreThroat from './questions/SymptomSoreThroat.js';
 import SymptomBodyPain from './questions/SymptomBodyPain.js';
 import SymptomCough from './questions/SymptomCough.js';
+import Http from '../../services/Http';
+import { checkup } from '../../constants/AppSettings';
 
 const formInitValues = {
   policyRead: false,
@@ -42,10 +44,12 @@ const formInitValues = {
 
 const screens = [
   {
+    id: 'Introduction',
     title: 'Introduction',
     component: Introduction
   },
   {
+    id: 'Terms',
     title: 'Introduction',
     component: Terms,
     questions: [
@@ -137,82 +141,122 @@ const screens = [
 export default function CheckupScreen() {
   const viewPager = useRef(null);
   let [currentIndex, setCurrentIndex] = useState(0);
+  let [isLoading, setLoading] = useState(false);
+  let [result, setResult] = useState(null);
+
   let [formValues, setFormValues] = useState(formInitValues);
 
   let total = screens.length;
   let title = screens[currentIndex].title || 'Patient';
+
+  //#region page selection
   const goToPreviousStep = () => {
     var i = currentIndex - 1;
     if (i >= 0) {
-      viewPager.current.setPage(i);
       setCurrentIndex(i)
-    }
-  }
-  const setValues = (values) => {
-    if (values && values.length) {
-      values.forEach((v) => {
-        formValues[v.name] = v.value;
-        setFormValues(Object.assign({}, formValues));
-      })
+      viewPager.current.setPage(i);
     }
   }
   const goToNextStep = () => {
-    var i = currentIndex + 1;
+    let i = currentIndex + 1;
     if (i <= total - 1) {
+      setCurrentIndex(i)
       viewPager.current.setPage(i);
-      setTimeout(() => {
-        setCurrentIndex(i);
-      }, 100);
     }
   }
+  const onPageSelected = (e) => {
+    // const pos = e.nativeEvent.position;
+    // setTimeout(() => {
+    //   setCurrentIndex(pos)
+    // }, 500);
+  }
+
+  // TODO: submit all values
+  const submitForm = async () => {
+    setLoading(true);
+    let data = 0;
+    try {
+      let data = await Http.post(checkup, formValues)
+      console.log('data', data);
+      setResult(data);
+    } catch (res) {
+      // TODO: show erro info
+      console.log('error in submitting', res)
+      setResult(10);
+    }
+    setLoading(false);
+    goToNextStep();
+  }
+  //#endregion page selection
+
+  //#region values setting
+  const setValues = (values) => {
+    if (values && values.length) {
+      values.forEach((v) => {
+        if (v.value !== undefined) {
+          formValues[v.name] = v.value;
+          setFormValues(Object.assign({}, formValues));
+        }
+      })
+    }
+  }
+  //#endregion
+
   let progress = (currentIndex + 1) / (total)
   let displayNext = currentIndex < total - 2;
   let displaySubmit = currentIndex === total - 2;
   let displayPrevious = currentIndex > 0;
   let isNextDisabled = false;
-  if (currentIndex === 1 && formValues['policyRead'] === false) {
+  let currentComponent = screens[currentIndex];
+  if (currentComponent.id === 'Terms' && formValues['policyRead'] === false) {
     isNextDisabled = true
   }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.progressContainer}>
-        <Text>{title}</Text>
-        {
-          (Platform.OS === 'android')
-            ?
-            <ProgressBarAndroid progress={progress} styleAttr="Horizontal" indeterminate={false} style={styles.progressAnd} />
-            :
-            <ProgressViewIOS progress={progress} style={styles.progressIos} progressTintColor="#49D581" />
-        }
-      </View>
-      <View style={styles.content}>
-        <ViewPager style={{ flex: 1 }} initialPage={0} ref={viewPager} scrollEnabled={false}>
-          {screens.map((q, k) => {
-            let QScreen = q.component;
-            return <View key={k} style={{ flex: 1 }}>
-              <QScreen questions={q.questions} setValues={setValues} ></QScreen>
-            </View>
-          })}
-        </ViewPager>
-      </View>
-      <Separator />
-      <View style={styles.navigation}>
-        <View style={styles.back}>
-          {displayPrevious ? <Back
-            isActive={true}
-            goToPreviousStep={() => goToPreviousStep()}
-          /> : null}
+    isLoading ? <ActivityIndicator>
+    </ActivityIndicator> :
+      <View style={styles.container}>
+        <View style={styles.progressContainer}>
+          <Text>{title}</Text>
+          {
+            (Platform.OS === 'android')
+              ?
+              <ProgressBarAndroid progress={progress} styleAttr="Horizontal" indeterminate={false} style={styles.progressAnd} />
+              :
+              <ProgressViewIOS progress={progress} style={styles.progressIos} progressTintColor="#49D581" />
+          }
         </View>
-        {displayNext ? <View style={styles.next}>
-          <Next disabled={isNextDisabled}
-            goToNextStep={() => goToNextStep()}
-          />
-        </View> : null}
-        {displaySubmit ? <View>
-          <Submit onSubmit={() => { goToNextStep(); Alert.alert(JSON.stringify([formValues.age, formValues.height, formValues.weight])) }} />
-        </View> : null}
+        <View style={styles.content}>
+          <ViewPager
+            onPageSelected={onPageSelected}
+            style={{ flex: 1 }} animationsAreEnabled={true}
+            initialPage={0} ref={viewPager} scrollEnabled={false}>
+            {screens.map((q, k) => {
+              let QScreen = q.component;
+              return <View key={k} style={{ flex: 1 }}>
+                <QScreen result={result} questions={q.questions} setValues={setValues} ></QScreen>
+              </View>
+            })}
+          </ViewPager>
+        </View>
+        <Separator />
+        <View style={styles.navigation}>
+          <View style={styles.back}>
+            {displayPrevious ? <Back
+              isActive={true}
+              goToPreviousStep={() => goToPreviousStep()}
+            /> : null}
+          </View>
+          {displayNext ? <View style={styles.next}>
+            <Next disabled={isNextDisabled}
+              goToNextStep={() => goToNextStep()}
+            />
+          </View> : null}
+          {displaySubmit ? <View>
+            <Submit onSubmit={() => { submitForm() }} />
+          </View> : null}
+        </View>
       </View>
-    </View>
   );
 }
 
--- go-corona-app/screens/checkup/questions/CheckupResult.js
+++ go-corona-app/screens/checkup/questions/CheckupResult.js
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import VirusImg from '../../../assets/images/Virus';
+import VirusPercentageSVG from '../components/VirusPercentage';
 
 const CheckupResult = (props) => {
-  const percenatage = props.percenatage || 40;
+  const percenatage = props.result || 40;
   const p = (percenatage / 100) * 180;
+
+  const getResultMessgae = () => {
+    if (percenatage > 40) {
+      return `Please stay quarantined at home`;
+    } else {
+      return `Your mostly safe, but be careful`;
+    }
+  }
   return (<View style={styles.container}>
     <View style={{ flexDirection: 'row', paddingTop: 50, alignItems: 'center', justifyContent: 'flex-start' }}>
-      <View style={{ height: 180, justifyContent: 'flex-end', marginLeft:-50, paddingRight:30  }}>
+      <View style={{ height: 180, justifyContent: 'flex-end', marginLeft: -50, paddingRight: 30 }}>
         <Text style={styles.probabilty}>{`${percenatage}%`}</Text>
         <Text style={styles.probabiltyLabel}>probabilty</Text>
       </View>
-      <View height={180} >
-        <View style={{ zIndex: 200, height: 180 - p }}>
-          <VirusImg width={180} fill={'gray'} height={180}></VirusImg>
-        </View>
-        <View style={{ height: p, backgroundColor: '#D2C985' }}>
-        </View>
-      </View>
+      <VirusPercentageSVG result={props.result} width={180} height={180}></VirusPercentageSVG>
     </View>
     <View style={{ paddingTop: 40 }}>
-      <Text style={styles.quarantined}>Please stay quarantined at home </Text>
+      <Text style={styles.quarantined}> {getResultMessgae()} </Text>
     </View>
-  </View>)
+  </View >)
 }
 
 const styles = StyleSheet.create({
