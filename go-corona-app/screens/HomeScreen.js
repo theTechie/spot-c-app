96e3bc54001623ab50887e1267a8524e55a32e7c
theTechie <gagan.blr@gmail.com>
2020-04-26 20:26:10 +0530

    fix unhandled exception

--- go-corona-app/screens/HomeScreen.js
+++ go-corona-app/screens/HomeScreen.js
@@ -43,7 +43,7 @@ export default function HomeScreen() {
       await updateMapData()
     });
 
-    return () => focusSubscription.remove()
+    return () => focusSubscription && focusSubscription.remove && focusSubscription.remove()
   }, [navigation.isFocused])
 
   updateMapData = async () => {
