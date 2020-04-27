9a389c6c155e27c875d1c0442875af20fe4dbb5f
AO-DEV <ao-dev@Anjis-Mac.local>
2020-04-27 22:08:48 +0530

    setValueCheckupScreens

--- go-corona-app/screens/checkup/questions/SymptomBreathlessNess.js
+++ go-corona-app/screens/checkup/questions/SymptomBreathlessNess.js
@@ -4,148 +4,153 @@ import { Divider, RadioButton } from 'react-native-paper';
 import BreathlessnessImage from '../../../assets/images/Breathlessness.svg';
 
 
-export default function SymptomBreathlessNess() {
-    const [values, setValues] = useState('first')
-    const [valueNext, setValueNext] = useState('wlfob')
+export default function SymptomBreathlessNess(props) {
+  const [breathlessness, setBreathlessness] = useState('false')
+  const [valueNext, setValueNext] = useState('wlfob')
 
-    const symptoms = [
-        {
-          name: 'While lying flat on bed',
-          value: 'wlfob'
-          
-        },
-        {
-          name: 'At Night',
-          value: 'an'
-          
-        },
-        {
-          name: 'Breathlessness that comes and goes',
-          value: 'btcg'
-         
-        },
-        {
-            name: 'Breathlessness while exercising',
-            value: 'bwe'
-           
-          },
-        {
-            name: 'Sudden feeling of breathlessness',
-            value: 'sfb'
-           
-         },
-        
-      ];
+  const setBreathlessnessValue = (v) => {
+    props.setValues({ breathlessness: v == 'true' });
+    setBreathlessness(v);
+  }
 
-    
+  const symptoms = [
+    {
+      name: 'While lying flat on bed',
+      value: 'wlfob'
+
+    },
+    {
+      name: 'At Night',
+      value: 'an'
+
+    },
+    {
+      name: 'Breathlessness that comes and goes',
+      value: 'btcg'
+
+    },
+    {
+      name: 'Breathlessness while exercising',
+      value: 'bwe'
+
+    },
+    {
+      name: 'Sudden feeling of breathlessness',
+      value: 'sfb'
+
+    },
+
+  ];
+
+
   return (
     <ScrollView>
-    <View style={styles.viewContainer}>
-            <View style={styles.sectionContainer}>
-            <Text style={styles.title}>Please tell us about your symptoms</Text>
-            <BreathlessnessImage  style={styles.image} width="200" height="120" />
-            </View>
-          <Text style={styles.textSty}>Are you feeling breathless?</Text> 
-          <Divider  />
-          <View  style={styles.agreeContainer}>
+      <View style={styles.viewContainer}>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.title}>Please tell us about your symptoms</Text>
+          <BreathlessnessImage style={styles.image} width="200" height="120" />
+        </View>
+        <Text style={styles.textSty}>Are you feeling breathless?</Text>
+        <Divider />
+        <View style={styles.agreeContainer}>
           <RadioButton.Group
-          onValueChange={values => setValues(values)}
-          value={values}
-           >
-          <View style={styles.agreeContainer}>
-          <View style={styles.radAlign}>
-           <RadioButton.Android value="first" color="#E03D51" uncheckedColor="#D2D2D2" />
-           <Text style={styles.radTxt}>Yes</Text>
-           </View>
-           <Divider  />
-          </View>
-          <View  style={styles.agreeContainer}>
-            <View style={styles.radAlign}>
-            <RadioButton.Android value="second" color="#E03D51" uncheckedColor="#D2D2D2" />
-            <Text style={styles.radTxt}>No</Text>
+            onValueChange={values => setBreathlessnessValue(values)}
+            value={breathlessness}
+          >
+            <View style={styles.agreeContainer}>
+              <View style={styles.radAlign}>
+                <RadioButton.Android value="true" color="#E03D51" uncheckedColor="#D2D2D2" />
+                <Text style={styles.radTxt}>Yes</Text>
+              </View>
+              <Divider />
             </View>
-            <Divider />
-          </View>
-        </RadioButton.Group>
+            <View style={styles.agreeContainer}>
+              <View style={styles.radAlign}>
+                <RadioButton.Android value="false" color="#E03D51" uncheckedColor="#D2D2D2" />
+                <Text style={styles.radTxt}>No</Text>
+              </View>
+              <Divider />
+            </View>
+          </RadioButton.Group>
         </View>
-        <Text style={styles.textSty}>If yes, select one of the following?</Text> 
-        <View  style={styles.agreeContainer}>
-        <RadioButton.Group
-        onValueChange={valueNext => setValueNext(valueNext)}
-        value={valueNext}
-         >
-        { symptoms.map((item)=>(
-        <View style={styles.agreeContainer} key={item.value}>
-        <View style={styles.radAlign} key={item.value}>
-         <RadioButton.Android value={item.value} color="#E03D51" uncheckedColor="#D2D2D2" />
-         <Text style={styles.radTxt}>{item.name}</Text>
-         </View>
-         <Divider  />
-        </View>
+        <Text style={styles.textSty}>If yes, select one of the following?</Text>
+        <View style={styles.agreeContainer}>
+          <RadioButton.Group
+            onValueChange={valueNext => setValueNext(valueNext)}
+            value={valueNext}
+          >
+            {symptoms.map((item) => (
+              <View style={styles.agreeContainer} key={item.value}>
+                <View style={styles.radAlign} key={item.value}>
+                  <RadioButton.Android value={item.value} color="#E03D51" uncheckedColor="#D2D2D2" />
+                  <Text style={styles.radTxt}>{item.name}</Text>
+                </View>
+                <Divider />
+              </View>
             )
             )}
-      </RadioButton.Group>
-        
+          </RadioButton.Group>
+
+        </View>
+
+
       </View>
-         
-        
-    </View>
-</ScrollView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   viewContainer: {
-      flex: 1,
-      backgroundColor: '#fafafa',
-      justifyContent: 'flex-start'
+    flex: 1,
+    backgroundColor: '#fafafa',
+    justifyContent: 'flex-start'
   },
   container: {
-      justifyContent: 'center'
+    justifyContent: 'center'
   },
-  contStyle:{
+  contStyle: {
     borderWidth: 0
   },
   sectionContainer: {
-      marginTop: 40,
-      marginBottom: 5,
-      alignItems: 'center',
+    marginTop: 40,
+    marginBottom: 5,
+    alignItems: 'center',
   },
   title: {
-      fontSize: 18,
-      fontWeight: "bold",
-      marginLeft:10,
-      marginBottom:25
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 10,
+    marginBottom: 25
   },
   helpLinks: {
-      marginTop: 30,
-      padding: 5,
-      width: "90%"
+    marginTop: 30,
+    padding: 5,
+    width: "90%"
   },
-  subTitle:{
-    alignSelf:'flex-start', 
-    marginLeft:25,
+  subTitle: {
+    alignSelf: 'flex-start',
+    marginLeft: 25,
   },
   agreeContainer: {
     marginLeft: 10,
     marginRight: 10,
-    flexDirection:'column'
+    flexDirection: 'column'
   },
-  txtStyle:{
+  txtStyle: {
     fontWeight: 'normal'
   },
-  textSty:{
-    marginLeft:24,
-    marginBottom:20,
+  textSty: {
+    marginLeft: 24,
+    marginBottom: 20,
     marginTop: 40
   },
-  radAlign:{
-    flexDirection:'row'
+  radAlign: {
+    flexDirection: 'row'
   },
-  radTxt:{
-    marginTop:8
+  radTxt: {
+    marginTop: 8
   },
-  image:{
+  image: {
     alignSelf: 'center',
     marginTop: 10
   }
--- go-corona-app/screens/checkup/questions/SymptomFever.js
+++ go-corona-app/screens/checkup/questions/SymptomFever.js
@@ -4,143 +4,147 @@ import { Divider, RadioButton } from 'react-native-paper';
 import Congestion from '../../../assets/images/Congestion.svg';
 
 
-export default function SymptomFever() {
-    const [values, setValues] = useState('first')
-    const [valueNext, setValueNext] = useState('ftcg')
-    const [yesNo,setYesNo] = useState('y')
-    
+export default function SymptomFever(props) {
+  const [fever, setFever] = useState('false')
+  const [valueNext, setValueNext] = useState('ftcg')
+  const [yesNo, setYesNo] = useState('y')
+  const setFeverValue = (v) => {
+    props.setValues({ fever: v === 'true' });
+    setFever(v);
+  }
+
   return (
     <ScrollView>
-    <View style={styles.viewContainer}>
-            <View style={styles.sectionContainer}>
-            <Text style={styles.title}>Please tell us about your symptoms</Text>
-            <Congestion style={styles.image} width="200" height="120" />
-            </View>
-          <Text style={styles.textSty}>Do you have fever?</Text> 
-          <Divider  />
-          <View  style={styles.agreeContainer}>
-          <RadioButton.Group
-          onValueChange={values => setValues(values)}
-          value={values}
-           >
-          <View style={styles.agreeContainer}>
-          <View style={styles.radAlign}>
-           <RadioButton.Android value="first" color="#E03D51" uncheckedColor="#D2D2D2" />
-           <Text style={styles.radTxt}>Yes</Text>
-           </View>
-           <Divider  />
-          </View>
-          <View  style={styles.agreeContainer}>
-            <View style={styles.radAlign}>
-            <RadioButton.Android value="second" color="#E03D51" uncheckedColor="#D2D2D2" />
-            <Text style={styles.radTxt}>No</Text>
-            </View>
-            <Divider />
-          </View>
-        </RadioButton.Group>
+      <View style={styles.viewContainer}>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.title}>Please tell us about your symptoms</Text>
+          <Congestion style={styles.image} width="200" height="120" />
         </View>
-        <Text style={styles.textSty}>Can you describe your fever?</Text> 
-        <View  style={styles.agreeContainer}>
-        <RadioButton.Group
-        onValueChange={valueNext => setValueNext(valueNext)}
-        value={valueNext}
-         >
-         <View style={styles.agreeContainer}>
-         <View style={styles.radAlign}>
-          <RadioButton.Android value="ftcg" color="#E03D51" uncheckedColor="#D2D2D2" />
-          <Text style={styles.radTxt}>Fever that comes and goes</Text>
-          </View>
-          <Divider  />
-         </View>
-        <View  style={styles.agreeContainer}>
-          <View style={styles.radAlign}>
-          <RadioButton.Android value="ftis" color="#E03D51" uncheckedColor="#D2D2D2" />
-          <Text style={styles.radTxt}>Fever that is consistent</Text>
-          </View>
-          <Divider />
-        </View>  
-      </RadioButton.Group>
-      </View>
-      <Text style={styles.textSty}>Have you measured your temperature?</Text> 
-      <View  style={styles.agreeContainer}>
-      <RadioButton.Group
-      onValueChange={yesNo => setYesNo(yesNo)}
-      value={yesNo}
-       >
-       <View style={styles.agreeContainer}>
-       <View style={styles.radAlign}>
-        <RadioButton.Android value="y" color="#E03D51" uncheckedColor="#D2D2D2" />
-        <Text style={styles.radTxt}>Yes</Text>
-        </View>
-        <Divider  />
-       </View>
-      <View  style={styles.agreeContainer}>
-        <View style={styles.radAlign}>
-        <RadioButton.Android value="n" color="#E03D51" uncheckedColor="#D2D2D2" />
-        <Text style={styles.radTxt}>No</Text>
-        </View>
+        <Text style={styles.textSty}>Do you have fever?</Text>
         <Divider />
-      </View> 
-    </RadioButton.Group>
-    </View>
-    <Text style={styles.textSty}>What's the temperature of your fever?</Text> 
-    </View>
-</ScrollView>
+        <View style={styles.agreeContainer}>
+          <RadioButton.Group
+            onValueChange={values => setFeverValue(values)}
+            value={fever}
+          >
+            <View style={styles.agreeContainer}>
+              <View style={styles.radAlign}>
+                <RadioButton.Android value="true" color="#E03D51" uncheckedColor="#D2D2D2" />
+                <Text style={styles.radTxt}>Yes</Text>
+              </View>
+              <Divider />
+            </View>
+            <View style={styles.agreeContainer}>
+              <View style={styles.radAlign}>
+                <RadioButton.Android value="false" color="#E03D51" uncheckedColor="#D2D2D2" />
+                <Text style={styles.radTxt}>No</Text>
+              </View>
+              <Divider />
+            </View>
+          </RadioButton.Group>
+        </View>
+        <Text style={styles.textSty}>Can you describe your fever?</Text>
+        <View style={styles.agreeContainer}>
+          <RadioButton.Group
+            onValueChange={valueNext => setValueNext(valueNext)}
+            value={valueNext}
+          >
+            <View style={styles.agreeContainer}>
+              <View style={styles.radAlign}>
+                <RadioButton.Android value="ftcg" color="#E03D51" uncheckedColor="#D2D2D2" />
+                <Text style={styles.radTxt}>Fever that comes and goes</Text>
+              </View>
+              <Divider />
+            </View>
+            <View style={styles.agreeContainer}>
+              <View style={styles.radAlign}>
+                <RadioButton.Android value="ftis" color="#E03D51" uncheckedColor="#D2D2D2" />
+                <Text style={styles.radTxt}>Fever that is consistent</Text>
+              </View>
+              <Divider />
+            </View>
+          </RadioButton.Group>
+        </View>
+        <Text style={styles.textSty}>Have you measured your temperature?</Text>
+        <View style={styles.agreeContainer}>
+          <RadioButton.Group
+            onValueChange={yesNo => setYesNo(yesNo)}
+            value={yesNo}
+          >
+            <View style={styles.agreeContainer}>
+              <View style={styles.radAlign}>
+                <RadioButton.Android value="y" color="#E03D51" uncheckedColor="#D2D2D2" />
+                <Text style={styles.radTxt}>Yes</Text>
+              </View>
+              <Divider />
+            </View>
+            <View style={styles.agreeContainer}>
+              <View style={styles.radAlign}>
+                <RadioButton.Android value="n" color="#E03D51" uncheckedColor="#D2D2D2" />
+                <Text style={styles.radTxt}>No</Text>
+              </View>
+              <Divider />
+            </View>
+          </RadioButton.Group>
+        </View>
+        <Text style={styles.textSty}>What's the temperature of your fever?</Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   viewContainer: {
-      flex: 1,
-      backgroundColor: '#fafafa',
-      justifyContent: 'flex-start'
+    flex: 1,
+    backgroundColor: '#fafafa',
+    justifyContent: 'flex-start'
   },
   container: {
-      justifyContent: 'center'
+    justifyContent: 'center'
   },
-  contStyle:{
+  contStyle: {
     borderWidth: 0
   },
   sectionContainer: {
-      marginTop: 40,
-      marginBottom: 5,
-      alignItems: 'center',
+    marginTop: 40,
+    marginBottom: 5,
+    alignItems: 'center',
   },
   title: {
-      fontSize: 18,
-      fontWeight: "bold",
-      marginLeft:10,
-      marginBottom:25
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 10,
+    marginBottom: 25
   },
   helpLinks: {
-      marginTop: 30,
-      padding: 5,
-      width: "90%"
+    marginTop: 30,
+    padding: 5,
+    width: "90%"
   },
-  subTitle:{
-    alignSelf:'flex-start', 
-    marginLeft:25,
+  subTitle: {
+    alignSelf: 'flex-start',
+    marginLeft: 25,
   },
   agreeContainer: {
     marginLeft: 10,
     marginRight: 10,
-    flexDirection:'column'
+    flexDirection: 'column'
   },
-  txtStyle:{
+  txtStyle: {
     fontWeight: 'normal'
   },
-  textSty:{
-    marginLeft:24,
-    marginBottom:20,
+  textSty: {
+    marginLeft: 24,
+    marginBottom: 20,
     marginTop: 40
   },
-  radAlign:{
-    flexDirection:'row'
+  radAlign: {
+    flexDirection: 'row'
   },
-  radTxt:{
-    marginTop:8
+  radTxt: {
+    marginTop: 8
   },
-  image:{
+  image: {
     alignSelf: 'center',
     marginTop: 10
   }
